24e24adb2cc4c67c2640f103facc38ed5419304a
Paul Harrsion <domfam60@gmail.com>
2026-04-15 00:59:00 +0900

    signup button fixed

--- public/script/index.js
+++ public/script/index.js
@@ -1,14 +1,16 @@
 const login = $(".loginmodal");
 const signup = $(".signupmodal");
 const loginbtn = $(".openlogin");
-const signupbtn = $(".openSignup");
+const signupbtn = $$(".openSignup");
 
 loginbtn.onclick = () => {
     login.classList.toggle("active");
     signup.classList.remove("active");
 }
 
-signupbtn.onclick = () => {
-    signup.classList.toggle("active");
-    login.classList.remove("active");
-}
+signupbtn.forEach(btn => {
+    btn.onclick = () => {
+        signup.classList.toggle("active");
+        login.classList.remove("active");
+    }
+})
